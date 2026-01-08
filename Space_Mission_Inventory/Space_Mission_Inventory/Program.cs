72aff47b49a5af0cd2f08f4721d0e968fc49b080
Gustavo020sp <gustavo020sp@gmail.com>
2026-01-08 15:01:10 -0300

    Add Space Mission Inventory Project

---
+++ Space_Mission_Inventory/Space_Mission_Inventory/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Space_Mission_Inventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
