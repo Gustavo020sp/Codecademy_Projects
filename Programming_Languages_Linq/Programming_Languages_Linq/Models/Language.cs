c377858fe80e3a0aaa5c13bfa28c68f0f7bd5260
Gustavo020sp <gustavo020sp@gmail.com>
2026-01-09 13:15:14 -0300

    linq exercises

---
+++ Programming_Languages_Linq/Programming_Languages_Linq/Models/Language.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programming_Languages_Linq.Models
+{
+    internal class Language
+    {
+        public static Language FromTsv(string tsvLine)
+        {
+            string[] values = tsvLine.Split('\t');
+
+            return new Language(
+                Convert.ToInt32(values[0]),
+                values[1],
+                values[2],
+                values[3]);
+        }
+
+        public int Year { get; set; }
+        public string Name { get; set; }
+        public string ChiefDeveloper { get; set; }
+        public string Predecessors { get; set; }
+
+        public Language(int year, string name, string chiefDeveloper, string predecessors)
+        {
+            Year = year;
+            Name = name;
+            ChiefDeveloper = chiefDeveloper;
+            Predecessors = predecessors;
+        }
+
+        public string Prettify()
+        {
+            return $"{Year}, {Name}, {ChiefDeveloper}, {Predecessors}";
+        }
+    }
+}
---
+++ Programming_Languages_Linq/Programming_Languages_Linq/Program.cs
@@ -0,0 +1,55 @@
+﻿using Programming_Languages_Linq.Models;
+
+namespace Programming_Languages_Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Language> languages = File.ReadAllLines("languages.tsv")
+        .Skip(1)
+        .Select(line => Language.FromTsv(line))
+        .ToList();
+
+            /*foreach (var item in languages)
+            {
+                Console.WriteLine(item.Prettify());
+            }*/
+
+            /*var csharp = languages.Where(l => l.Name == "C#").ToList();
+            foreach (var item in csharp)
+            {
+                Console.WriteLine(item.Prettify());
+            }*/
+
+            /*var microsoft = languages.Where(l => l.ChiefDeveloper == "Microsoft").ToList();
+            foreach (var item in microsoft)
+            {
+                Console.WriteLine(item.Prettify());
+            }*/
+
+            /*var lispdesc = languages.Where(l => l.Predecessors == "Lisp").ToList();
+            foreach (var item in lispdesc)
+            {
+                Console.WriteLine(item.Prettify());
+            }*/
+
+            /*var containscript = languages.Where(l => l.Name.Contains("Script")).ToList();
+            foreach (var item in containscript)
+            {
+                Console.WriteLine(item.Prettify());
+            }*/
+
+            /*int totallanguages = languages.Count;
+            Console.WriteLine(totallanguages);*/
+
+            var languagesearch = languages.Where(l => l.Year > 1994 && l.Year < 2006);
+            int countsearch = languagesearch.Count();
+            Console.WriteLine($"Total Languages between 1995 and 2005: {countsearch}");
+
+
+
+
+        }
+    }
+}
